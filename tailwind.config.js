dacb1a6db2163ea3c65507fe94cda034ad8ebe5e
AlexRS90 <aramosbengy@gmail.com>
2022-04-26 12:26:04 -0500

    Add poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
     extend: {
       colors: {
         primary: '#6070FF',
+        second: '#EBEBFF',
       },
       backgroundImage: {
         'hero-pattern': "url('/src/assets/Header_bg.svg')",
       },
       borderRadius: {
         extraLarge: '12rem',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
       },
     },
     plugins: [],
