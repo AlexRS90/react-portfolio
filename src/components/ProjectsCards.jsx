c2ca50c64b3af0a27b00497bb516de812f811532
AlexRS90 <aramosbengy@gmail.com>
2022-04-26 12:26:25 -0500

    Create cards to display projects

--- src/components/ProjectsCards.jsx
+++ src/components/ProjectsCards.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 import projects from '../assets/projects.json';
 
 const Projects = () => {
-  console.log(projects);
+  console.table(projects);
   return (
-    <section>
-      <img src={projects[3].picture} alt="twitter" />
+    <section className="m-5 lg:m-36">
+      <ul>
+        <li className="flex flex-col rounded-xl bg-stone-50 p-5 gap-4">
+          <img
+            src={projects[0].picture}
+            alt={`${projects[0].title} project`}
+            className="border border-primary rounded-xl"
+          />
+          <p className="text-3xl">{projects[0].title}</p>
+          <p>{projects[0].description}</p>
+          <ul className="flex justify-between md:w-80">
+            {
+              projects[0].tecnhologies.map((tec) => (
+                <li key="1" className="bg-second p-2 rounded-md text-primary">
+                  <p>{tec}</p>
+                </li>
+              ))
+            }
+          </ul>
+          <button
+            type="button"
+            className="border border-primary w-32 rounded-md p-2 text-center text-primary"
+          >
+            See Project
+          </button>
+        </li>
+      </ul>
     </section>
   );
 };
