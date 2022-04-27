a21925dc6c06fa99a24ef73d3d05cd6b4acf2dc2
AlexRS90 <aramosbengy@gmail.com>
2022-04-27 18:11:58 -0500

    Add new component to app file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import NavBar from './components/Navbar';
 import MainView from './components/Main';
 import Projects from './components/ProjectsCards';
+import About from './components/About';
 
 const App = () => (
   <>
     <NavBar />
     <MainView />
     <Projects />
+    <About />
   </>
 );
 
