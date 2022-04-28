b1f6d5119767567f5d0dbcc0344b4a55fbc82c2c
AlexRS90 <aramosbengy@gmail.com>
2022-04-28 17:58:00 -0500

    Add skills list

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 const TechStack = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenFrame, setIsOpenFrame] = useState(false);
+  const [isOpenSkills, setIsOpenSkills] = useState(false);
   const languages = ['JavaScript', 'Ruby', 'HTML/CSS', 'PostgreSQL'];
   const frameworks = ['React', 'Ruby on Rails', 'Tailwind', 'Bootstrap', 'RSpec', 'CapyBara'];
-  // const skills = ['Git/GitHub', 'Terminal', 'Mobile First', 'OOP', 'TDD'];
+  const skills = ['Git/GitHub', 'Terminal', 'Mobile First', 'OOP', 'TDD'];
   return (
     <div>
       <button
@@ -48,6 +49,28 @@ const TechStack = () => {
               key="1"
             >
               {frame}
+            </li>
+          ))
+        }
+      </ul>
+      <button
+        type="button"
+        onClick={() => setIsOpenSkills(!isOpenSkills)}
+        className="w-full mt-10 p-5 flex justify-between border-b-2 border-background text-xl"
+      >
+        Skills
+        <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${isOpenSkills ? 'rotate-90' : 'rotate-0'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+      </button>
+      <ul className={`${isOpenSkills ? 'block' : 'hidden'}`}>
+        {
+          skills.map((skill) => (
+            <li
+              className="w-9/12 bg-background m-2 p-8"
+              key="1"
+            >
+              {skill}
             </li>
           ))
         }
