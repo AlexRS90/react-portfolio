131fe319aa3cb7a418859d95145ca13f22e9e976
AlexRS90 <aramosbengy@gmail.com>
2022-04-28 13:23:03 -0500

    Add Arrow icon

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -10,6 +10,9 @@ const TechStack = () => {
         className={`${isOpen ? 'bg-primary' : 'bg-red-500'} `}
       >
         Languages
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
       </button>
     </div>
   );
