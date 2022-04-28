71a6e6a16491ec8b032795af70c120e332c9f2d1
AlexRS90 <aramosbengy@gmail.com>
2022-04-28 12:53:29 -0500

    Create new component for the dropdown lists

---
+++ src/components/TechStack.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const TechStack = () => {
+  const [state, newState] = useState(false);
+  return (
+    <div>
+      <button type="button">Languages</button>
+    </div>
+  );
+};
+
+export default TechStack;
