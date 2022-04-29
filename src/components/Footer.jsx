2eda7b2410412db71a4818b00daede244075763d
AlexRS90 <aramosbengy@gmail.com>
2022-04-29 17:23:15 -0500

    Give some style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+  const pumas = 'Concacaf Champion';
   return (
-    <footer></footer>
+    <footer className="bg-primary rounded-tl-extraLarge h-80">
+      <p>{pumas}</p>
+    </footer>
   );
 };
 
