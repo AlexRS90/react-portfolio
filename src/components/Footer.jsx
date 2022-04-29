d271e3609afe868c7475be6a66ac797832335222
AlexRS90 <aramosbengy@gmail.com>
2022-04-29 17:58:25 -0500

    Add bg classes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Footer = () => {
   const pumas = 'Concacaf Champion';
   return (
-    <footer className="bg-primary rounded-tl-extraLarge h-80">
-      <p>{pumas}</p>
+    <footer className="bg-primary rounded-tl-extraLarge p-20">
+      <div className="bg-footer-pattern bg-no-repeat h-screen">
+        <p>{pumas}</p>
+      </div>
     </footer>
   );
 };
